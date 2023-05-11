7f9ed5b0ff88c7e032736be6ea22fce544ed8662
Christopher Black <chris@primeacademy.io>
2023-05-11 17:45:14 -0500

    Added code to test the tests.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,7 +1,12 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
 
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
 
 
 // Do not modify, used for automated testing
