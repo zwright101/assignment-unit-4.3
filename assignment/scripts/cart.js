16160055f047ca34d64e8c7a2a8be938922b300f
Christopher Black <chris@primeacademy.io>
2023-05-11 19:16:12 -0500

    Added test for listItems.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,18 +1,25 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
 
-function addItem(item) {
-    basket.push(item);
-    return true;
+
+
+
+
+
+
+
+
+// DO NOT MODIFY
+// Used for automated testing
+try {
+    module.exports = {
+        basket: typeof basket !== 'undefined' ? basket : undefined,
+        addItem: typeof addItem !== 'undefined' ? addItem : undefined,
+        listItems: typeof listItems !== 'undefined' ? listItems : undefined,
+        empty: typeof empty !== 'undefined' ? empty : undefined,
+        isFull: typeof isFull !== 'undefined' ? isFull : undefined,
+    };
+} catch(e) {
+    // Do nothing
 }
-
-
-// Do not modify, used for automated testing
-module.exports = {
-    basket,
-    addItem,
-    // listItems,
-    // empty,
-};
--- test/test.js
+++ test/test.js
@@ -1,32 +1,40 @@
-let assert;
-let expect;
+/**
+ * We have to do a bit of work upfront to allow the tests
+ * to run in the browser and in Node.js. 
+ */
+let assert, expect;
+let testItems = {};
 if (typeof window === 'object') {
     // Run tests in browser
     assert = chai.assert;
     expect = chai.expect;
-    // 'bdd' stands for "behavior driven development"
     mocha.setup('bdd');
+    testItems = {
+        basket: typeof basket !== 'undefined' ? basket : undefined,
+        addItem: typeof addItem !== 'undefined' ? addItem : undefined,
+        listItems: typeof listItems !== 'undefined' ? listItems : undefined,
+        empty: typeof empty !== 'undefined' ? empty : undefined,
+    };
 } else {
     // Run tests in Node.js
     assert = require('assert');
     expect = require('chai').expect;
-
-    const {
-        basket,
-        addItem,
-        listItems,
-        empty,
-    } = require('../assignment/scripts/cart.js');
+    testItems = require('../assignment/scripts/cart.js');
 }
 
+/**
+ * Put all tests within this describe.
+ */
 describe('Automated tests', function () {
     describe('Created global variable for `basket` as empty array', function () {
         it('Created global variable for `basket` as empty array', function () {
+            let { basket } = testItems;
             expect(basket).to.be.a('array');
         });
     });
     describe('`addItem` function takes in an item, adds to the array', function () {
         it('`addItem` function takes in an item, adds to the array', function () {
+            let { basket, addItem } = testItems;
             addItem('Kale');
             expect(basket.length).to.be.greaterThan(0);
             assert.equal(basket[basket.length - 1], 'Kale');
@@ -34,13 +42,37 @@ describe('Automated tests', function () {
     });
     describe('`addItem` function returns true', function () {
         it('`addItem` function returns true', function () {
+            let { addItem } = testItems;
             const result = addItem('Kale');
             expect(result, 'addItem() does not return anything').to.exist;
             expect(result).to.be.a('boolean');
             assert.equal(result, true);
         });
     });
+    describe('`listItems` loops over `basket` array and logs each item', function () {
+        it('`listItems` loops over `basket` array and logs each item', function () {
+            let { listItems } = testItems;
+            // clear basket
+            basket.length = 0;
+            basket.push('Kale', 'Spinach');
+            let tempLog = console.log;
+            let result = '';
+            // Temporarily override console.log
+            console.log = (item) => result += item;
+            listItems(basket);
+            // Set it back to default
+            console.log = tempLog;
+            expect(result, 'listItems() does not return anything').to.exist;
+            expect(result).to.be.a('string');
+            assert.equal(result.includes('Kale'), true);
+            assert.equal(result.includes('Spinach'), true);
+        });
+    });
 });
+
+/**
+ * If running the tests in the browser, call mocha.run()
+ */
 if (typeof window === 'object') {
     mocha.run();
 }
