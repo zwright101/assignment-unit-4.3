88843c386b46e7452553ccac6cc6bfedaeee37af
Zach Wright <zwright101@gmail.com>
2023-06-22 20:44:31 -0500

    finished the main points and a few of the stretch goals.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,11 +3,54 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 
+let basket = []; 
+
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+addItem('Apple');
+addItem('Bananas');
+addItem('Kale');
+addItem('Spinach');
+addItem('Eggs');
+addItem('Bread');
+addItem('Kiwi');
+
+console.log(basket);
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+
+function listItems(basket){
+    for (let i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
+    }
+  }
+
+listItems(basket);
+
+
+//function empty(array){
+//    array.splice(0, array.length)
+// }
+// empty(basket);
+// console.log(basket); 
 
 
 
 
+const maxItems = 5;
 
+function isFull(array) {
+    if (array >= maxItems) {
+        return true;
+    } else {
+        return false;
+    }
+}
+isFull(basket);
 
 
 
